3e711dc42169248cf36dc0700263de7fea441f6e
Midori Bowen and Patrick Sullivan <midori.p.bowen@gmail.com>
2016-02-10 16:20:50 -0800

    update the readme

--- README.md
+++ README.md
@@ -1,37 +1,24 @@
-# _{Application Name}_
+# Tic Tac Toe
 
-#### _{Brief description of application}, {Date of current version}_
-
-#### By _**{List of contributors}**_
+#### best tic tac toe
 
 ## Description
 
-_{This is a detailed description of your application. Its purpose and usage.  Give as much detail as needed to explain what the application does, and any other information you want users or other developers to have. }_
-
-## Setup/Installation Requirements
-
-* _This is a great place_
-* _to list setup instructions_
-* _in a simple_
-* _easy-to-understand_
-* _format_
-
-_{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?}_
+it plays the tic tac toe
 
 ## Known Bugs
 
-_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
+player must make first move
+you have to click to have the cpu make a move.
+need tests for ai behavior
+no control over who goes first.
+alternates automatically between players.
+no support for ai finding forks.
+no support for ai making move on its own.
 
-## Support and contact details
-
-_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
-
-## Technologies Used
-
-_{Tell me about the languages and tools you used to create this app. Assume that I know you probably used HTML and CSS. If you did something really cool using only HTML, point that out.}_
 
 ### License
 
-*{Determine the license under which this application can be used.  See below for more details on licensing.}*
+MIT
 
-Copyright (c) 2015 **_{List of contributors or company name}_**
+Copyright (c) 2015
--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,115 @@ function Board() {
   this.moveCount = 0;
 }
 
+// player move
 Board.prototype.makeMove = function(player, yCoord, xCoord) {
   this.board[yCoord][xCoord] = player;
   this.moveCount++;
 }
 
+//ai logic starts here
+Board.prototype.cpuMove = function () {
+  var centerEmpty = this.center();
+  var cornerEmpty = this.cornerEmpty();
+  var twoInARow = this.twoInARow();
+  var yCoord;
+  var xCoord;
+  if (twoInARow !== false) {
+    yCoord = twoInARow[0];
+    xCoord = twoInARow[1];
+  } else if (centerEmpty) {
+    yCoord = 1;
+    xCoord = 1;
+  } else if (cornerEmpty !== false) {
+    yCoord = cornerEmpty[0];
+    xCoord = cornerEmpty[1];
+  } else {
+    yCoord = Math.floor(Math.random() * 3);
+    xCoord = Math.floor(Math.random() * 3);
+  }
+  console.log(yCoord);
+  this.board[yCoord][xCoord] = 2;
+  this.moveCount++;
+  return [yCoord,xCoord];
+};
+
+Board.prototype.twoInARow = function() {
+  twoHorizontal = this.twoHorizontal();
+  twoVertical = this.twoVertical();
+  twoDiagonal = this.twoDiagonal();
+  if (twoHorizontal !== false) {
+    return twoHorizontal;
+  } else if (twoVertical) {
+    return twoVertical;
+  } else if (twoDiagonal) {
+    return twoDiagonal;
+  } else {
+    return false;
+  }
+}
+
+Board.prototype.twoHorizontal = function() {
+  for(var i = 0; i < this.board.length; i++) {
+    if ((this.board[i][0] === this.board[i][1]) && (this.board[i][0] !== 0) && (this.board[i][2] === 0)) {
+      return [i, 2];
+    } else if ((this.board[i][1] === this.board[i][2]) && (this.board[i][1] !== 0) && (this.board[i][0] === 0)) {
+      return [i, 0];
+    } else if ((this.board[i][0] === this.board[i][2]) && (this.board[i][0] !== 0) && (this.board[i][1] === 0)) {
+      return [i, 1];
+    }
+  }
+  return false;
+}
+
+Board.prototype.twoVertical = function() {
+  for(var i = 0; i < this.board[0].length; i++) {
+    if ((this.board[0][i] === this.board[1][i]) && (this.board[0][i] !== 0) && (this.board[2][i] === 0)) {
+      return [2, i];
+    } else if ((this.board[1][i] === this.board[2][i]) && (this.board[1][i] !== 0) && (this.board[0][i] === 0)) {
+      return [0, i];
+    } else if ((this.board[0][i] === this.board[2][i]) && (this.board[0][i] !== 0) && (this.board[1][i] === 0)) {
+      return [1, i];
+    }
+  }
+  return false;
+}
+
+Board.prototype.twoDiagonal = function() {
+  if ((this.board[0][0] === this.board[1][1]) && (this.board[0][0] !== 0) && (this.board[2][2] === 0)) {
+    return [2, 2];
+  } else if ((this.board[1][1] === this.board[2][2]) && (this.board[1][1] !== 0) && (this.board[0][0] === 0)) {
+    return [0, 0];
+  } else if ((this.board[0][0] === this.board[2][2]) && (this.board[0][0] !== 0) && (this.board[1][1] === 0)) {
+    return [1, 1];
+  } else if ((this.board[0][2] === this.board[1][1]) && (this.board[0][2] !== 0) && (this.board[2][0] === 0)) {
+    return [2, 0];
+  } else if ((this.board[2][0] === this.board[1][1]) && (this.board[2][0] !== 0) && (this.board[0][2] === 0)) {
+    return [0, 2];
+  } else if ((this.board[2][0] === this.board[0][2]) && (this.board[2][0] !== 0) && (this.board[1][1] === 0)) {
+    return [1, 1];
+  }
+  return false;
+}
+
+Board.prototype.center = function() {
+  return this.board[1][1] === 0;
+}
+
+Board.prototype.cornerEmpty = function() {
+  if (this.board[0][0] === 0) {
+    return [0,0];
+  } else if (this.board[0][2] === 0) {
+    return [0,2];
+  } else if (this.board[2][0] === 0) {
+    return [2,0];
+  } else if (this.board[2][2] === 0) {
+    return [2,2];
+  } else {
+    return false;
+  }
+} //ai logic ends here
+
+// check all win conditions
 Board.prototype.checkWin = function () {
   var horizontalWin = this.checkHorizontal();
   var verticalWin = this.checkVertical();
@@ -87,9 +191,15 @@ $(function() {
     var coordinates = $(this).attr("id").split("");
     var yCoord = coordinates[0];
     var xCoord = coordinates[1];
-    board.makeMove(currentPlayer, parseInt(yCoord), parseInt(xCoord));
-    $(".tile#" + yCoord + xCoord).addClass("player" + currentPlayer);
-    $(".tile#" + yCoord + xCoord).attr("disabled", "true");
+    if (currentPlayer === 1) {
+      board.makeMove(currentPlayer, parseInt(yCoord), parseInt(xCoord));
+      $(".tile#" + yCoord + xCoord).addClass("player" + currentPlayer);
+      $(".tile#" + yCoord + xCoord).attr("disabled", "true");
+    } else {
+      var coords = board.cpuMove();
+      $(".tile#" + coords[0] + coords[1] ).addClass("player" + currentPlayer);
+      $(".tile#" + coords[0] + coords[1] ).attr("disabled", "true");
+    }
     var win = board.checkWin();
     var draw = board.checkDraw();
     if (win === true) {
